202ce68ae6866e17e1ffb62956115622c6d98cee
Donovan Crowley <donocrowley16@gmail.com>
2025-05-16 17:13:04 -0400

    Update Profile

--- app.js
+++ app.js
@@ -1,71 +1,87 @@
-const nav = document.querySelector(".nav");
-const navMenu = document.querySelector(".nav-items");
-const btnToggleNav = document.querySelector(".menu-btn");
-const workEls = document.querySelectorAll(".work-box");
-const workImgs = document.querySelectorAll(".work-img");
-const mainEl = document.querySelector("main");
-const yearEl = document.querySelector(".footer-text span");
+const modal1 = document.getElementById("portfolioModal1");
+const trigger1 = document.getElementById("project-modal1");
+const span1 = document.getElementsByClassName("close-btn1")[0];
+const modal2 = document.getElementById("portfolioModal2");
+const trigger2 = document.getElementById("project-modal2");
+const span2 = document.getElementsByClassName("close-btn2")[0];
 
-const toggleNav = () => {
-  nav.classList.toggle("hidden");
-
-  // Prevent screen from scrolling when menu is opened
-  document.body.classList.toggle("lock-screen");
-
-  if (nav.classList.contains("hidden")) {
-    btnToggleNav.textContent = "menu";
-  } else {
-    // When menu is opened after transition change text respectively
-    setTimeout(() => {
-      btnToggleNav.textContent = "close";
-    }, 475);
-  }
-};
-
-btnToggleNav.addEventListener("click", toggleNav);
-
-
-navMenu.addEventListener("click", (e) => {
-  if (e.target.localName === "a") {
-    toggleNav();
+// Set the background color of bookmark to black after scrolling
+window.addEventListener('scroll', function(){
+  const bookmark = document.querySelector('.bookmark');
+  if(window.scrollY > 50){
+    bookmark.classList.add('scrolled');
+  } else{
+    bookmark.classList.remove('scrolled');
   }
 });
 
-document.body.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && !nav.classList.contains("hidden")) {
-    toggleNav();
+// Open VTOL modal 
+trigger1.onclick = function(e) {
+  modal1.style.display = "block";
+  document.body.classList.add("lock-screen");
+  document.body.classList.add("body-dark");
+}
+
+// Close VTOL modal with x click
+span1.onclick = function() {
+  modal1.style.display = "none";
+  document.body.classList.remove("lock-screen");
+  document.body.classList.remove("body-dark");
+}
+
+// Close modals with outside click
+window.onclick = function(event) {
+  // Close VTOL
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+    document.body.classList.remove("lock-screen");
+    document.body.classList.remove("body-dark");
   }
-});
 
-// Animating work instances on scroll
+  // Close EV
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+    document.body.classList.remove("lock-screen");
+    document.body.classList.remove("body-dark");
+  }
+}
 
-workImgs.forEach((workImg) => workImg.classList.add("transform"));
+// Open EV modal
+trigger2.onclick = function(e) {
+  modal2.style.display = "block";
+  document.body.classList.add("lock-screen");
+  document.body.classList.add("body-dark");
+}
 
-let observer = new IntersectionObserver(
-  (entries) => {
-    const [entry] = entries;
-    const [textbox, picture] = Array.from(entry.target.children);
-    if (entry.isIntersecting) {
-      picture.classList.remove("transform");
-      Array.from(textbox.children).forEach(
-        (el) => (el.style.animationPlayState = "running")
-      );
+// Close EV modal with x click
+span2.onclick = function() {
+  modal2.style.display = "none";
+  document.body.classList.remove("lock-screen");
+  document.body.classList.remove("body-dark");
+}
+
+// When the screen is resized, use the menu option
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.querySelector(".menu-btn");
+  const links = document.querySelector(".bookmark-toggle-links");
+  const bookmark = document.querySelector(".bookmark");
+
+  // Open/Close menu
+  toggle.addEventListener("click", () => {
+    links.classList.toggle("open");
+  });
+
+  // Close menu when a link is clicked
+  links.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+      links.classList.remove("open");
+    });
+  });
+
+  // Close menu when clicking outside of it
+  document.addEventListener("click", function(event){
+    if (!bookmark.contains(event.target) && !links.contains(event.target) && !toggle.contains(event.target)) {
+      links.classList.remove("open");
     }
-  },
-  { threshold: 0.3 }
-);
-
-workEls.forEach((workEl) => {
-  observer.observe(workEl);
-});
-
-// Trap the tab when menu is opened
-
-const lastFocusedEl = document.querySelector('a[data-focused="last-focused"]');
-
-document.body.addEventListener("keydown", (e) => {
-  if (e.key === "Tab" && document.activeElement === lastFocusedEl) {
-    e.preventDefault();
-    btnToggleNav.focus();
-  }
+  })
 });
